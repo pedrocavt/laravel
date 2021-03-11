4c95a32022b9f272ccacfb30a4d885639c450cc9
Pedro Vitor <pedrocavt@gmail.com>
2021-03-11 08:25:53 -0300

    hdcevents

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -56,6 +57,9 @@ class EventController extends Controller
             $event->image = $imageName;
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', "Evento criado com sucesso!");
@@ -64,6 +68,8 @@ class EventController extends Controller
     public function show($id){
 
         $event = Event::findOrFail($id);
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
         return view('events.show', ['event' => $event]);
 
